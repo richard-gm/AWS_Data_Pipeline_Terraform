6a80c75371246ca5de56bde91fdaf047998012c3
Richard Guaman <rguamans@cisco.com>
2024-08-05 19:42:55 +0100

    refactoring kafka configuration

--- modules/kafka/variables.tf
+++ modules/kafka/variables.tf
@@ -17,3 +17,25 @@ variable "prefix" {
   description = "Resource name prefix"
   type        = string
 }
+
+variable "kafka_broker_count" {
+  description = "Number of Kafka broker instances"
+  type        = number
+  default     = 2
+}
+
+variable "kafka_ami" {
+  description = "AMI ID for Kafka instances"
+  type        = string
+}
+
+variable "kafka_instance_type" {
+  description = "Instance type for Kafka brokers"
+  type        = string
+  default     = "t3.medium"
+}
+
+variable "ssh_key_name" {
+  description = "SSH key name for EC2 instances"
+  type        = string
+}
